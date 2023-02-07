dffbe8a2bd47c776b0f0aff5f49dd5e082324fe4
marcio <marciobouhid@gmail.com>
2023-02-07 13:23:24 +0000

    calc adjusted

--- notebooks/pages/1_▶️_PEDIDOS.py
+++ notebooks/pages/1_▶️_PEDIDOS.py
@@ -1,23 +1,43 @@
 # Libs
-
-import streamlit as st
 import sqlite3
+import streamlit as st
+import calendar
 import pandas as pd
 
+# Connect DB
 conn = sqlite3.connect('pallet.db', check_same_thread=False)
 cur = conn.cursor()
-
 
 
 # Page Title
 st.title('PEDIDOS')
 
+# -Settings-
+models = ['pallet100', 'pallet120']
+sub_pallet120 = ['1200x90x15', '1000x90x18', '90x90x90', 'extra1200x90x15']
+months = list(calendar.month_name[1:])
+
+
 
 def form():
     st.write('Preencher com as informações para o cálculo')
     with st.form(key='Cálculo de materiais', clear_on_submit=True):
-        name = st.text_input('Nome do Produto:')
-        qtd = st.text_input('Quantidade:')
+        name = st.text_input('Nome do Cliente:')
+        col1, col2 = st.columns(2)
+        with col1:
+            qtd = st.sidebar.number_input('Quantidade:', 100, step=100)
+        with col2:
+            month_dlv = st.sidebar.selectbox('Mês de entrega:', months)
+        with st.expander('Pallet120'):
+            for prod in sub_pallet120:
+                if prod == '1200x90x15':
+                    st.write(f'{prod}:', qtd*8)
+                elif prod == '1000x90x18':
+                    st.write(f'{prod}:', qtd*3)
+                elif prod == '90x90x90':
+                    st.write(f'{prod}:', qtd*9)
+                elif prod == 'extra1200x90x15':
+                    st.write(f'{prod}:', qtd*3)
         col1, col2 = st.columns(2)
         with col1:
             submission = st.form_submit_button(label='Salvar')
@@ -44,20 +64,25 @@ query = """
     select * from pedido
     
 """
-
 df = pd.read_sql_query( query, conn)
-st.write(df)
+#st.write(df)
 
-st.sidebar.multiselect(
-    'Escolha um produto',
-    list(df['nome_produto'].unique()), 
-    default=list(df['nome_produto'].unique())
-)
+## Filters
+#st.sidebar.multiselect(
+#    'Escolha um produto',
+#    list(df['nome_produto'].unique()), 
+#    default=list(df['nome_produto'].unique())
+#)
+#
+#st.sidebar.multiselect(
+#    'Escolha os meses',
+#    months, 
+#)
 
 
-df['qtd'] = df['qtd'].astype(int)
-df_filtered = df[df['nome_produto'] == 'test2']
-texto = 'A soma do test2 é : '
-st.write(texto, df_filtered['qtd'].sum())
+#df['qtd'] = df['qtd'].astype(int)
+#df_filtered = df[df['nome_produto'] == 'test2']
+#texto = 'A soma do test2 é : '
+#st.write(texto, df_filtered['qtd'].sum())
 
 conn.close()
